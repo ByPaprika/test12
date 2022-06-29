989067c99689d04d0b7cf858553cad7b3388e6c2
Василий <49844634+ByPaprika@users.noreply.github.com>
2022-06-29 22:50:52 +0300

    a

--- src/main/java/com/bot/bot/config/WebSecurityConfig.java
+++ src/main/java/com/bot/bot/config/WebSecurityConfig.java
@@ -22,7 +22,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .mvcMatchers("/").permitAll()
+                .mvcMatchers("/", "/bot/ask").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable();
